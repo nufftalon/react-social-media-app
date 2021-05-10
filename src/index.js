d98f88cb68c9c76c569c098c788049c320f99b7a
nufftalon <hawktimewebdesign@gmail.com>
2021-05-10 00:18:29 -0400

    Starting social media react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const greeting = <div>Hello React</div>;
+const isReactUser = false;
+const rootNode = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (isReactUser){
+
+} else{
+    
+}
+function sayGreeting();
+
+ReactDOM.render(greeting, rootNode);
+

cf1951ef2b6ae39a84147f25f5427a59e835b625
nufftalon <hawktimewebdesign@gmail.com>
2021-05-10 23:59:18 -0400

    testing background color on layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const year = 2020;
-const greeting = <div>Hello React in {year}</div>;
+function Header(props){
+    return <h1>Hello {props.username}</h1>
+}
+
+function Layout(props){
+    return <div style={{background: 'palegoldenrod'}}>
+        {props.children}
+    </div>
+}
+
 const rootNode = document.getElementById('root');
 
 
 
-ReactDOM.render(greeting, rootNode);
+ReactDOM.render
+(<Layout>
+<Header username="Gary"/>
+<footer>Copyright 2020</footer>
+</Layout>, rootNode);
 

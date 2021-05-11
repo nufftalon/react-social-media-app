257a9e5aa5ccdcc8bad4589318170202b56f3083
nufftalon <hawktimewebdesign@gmail.com>
2021-05-11 01:08:06 -0400

    added input comp on change event

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import ReactDOM from 'react-dom';
 function App() {
   const people = ['John', 'Dave', 'Gary'];
 
+  function handleInputChange(event) {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+  }
+
   return (
     <ul>
-      {people.map((person) => (
-        <li>{person}</li>
+      {people.map((person, i) => (
+        <Person key={i} person={person} />
       ))}
+      <input onChange={handleInputChange} />
     </ul>
   );
+}
+
+function Person(props) {
+  function handlePersonClick(event) {
+    alert(props.person);
+  }
+  return <li onClick={handlePersonClick}>{props.person}</li>;
 }
 
 const rootNode = document.getElementById('root');

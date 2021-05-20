33b9246dc7d6323801caaf5eaa106443499fec02
nufftalon <hawktimewebdesign@gmail.com>
2021-05-19 20:41:30 -0400

    created function to handle employment state

--- src/index.js
+++ src/index.js
@@ -2,21 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  const [language, setLanguage] = React.useState('python');
-  const [yearsExperence, setYearsExperience] = React.useState(0);
+  const [developer, setDeveloper] = React.useState({
+    language: 'python',
+    yearsExperence: 0,
+  });
+  // const [language, setLanguage] = React.useState('python');
+  // const [yearsExperence, setYearsExperience] = React.useState(0);
+  function handleChangeLangauge() {
+    setDeveloper({
+      language: 'javascript',
+      yearsExperence: 0,
+      isEmployed: false,
+    });
+  }
+
+  function handleChangeYearsExperience(event) {
+    setDeveloper({
+      ...developer,
+      yearsExperence: event.target.value,
+    });
+  }
+
+  function handleToggleEmployment() {
+    setDeveloper((prevState) => ({
+      ...prevState,
+      isEmployed: !prevState.isEmployed,
+    }));
+  }
 
   return (
     <div>
-      <button onClick={() => setLanguage('javascript')}>Change language</button>
+      <button onClick={handleToggleEmployment}>Toggle Employment Status</button>
+      <button onClick={handleChangeLangauge}>Change language</button>
       <div>
-        <input
-          type="number"
-          onChange={(event) => setYearsExperience(event.target.value)}
-        />
+        <input type="number" onChange={handleChangeYearsExperience} />
       </div>
 
-      <p> I am learning {language}.</p>
-      <p>I have {yearsExperence} years of experience</p>
+      <p> I am learning {developer.language}.</p>
+      <p>I have {developer.yearsExperence} years of experience</p>
+      <p>
+        Employment status: {developer.isEmployed ? 'Employed' : 'Unemployed'}
+      </p>
     </div>
   );
 }
